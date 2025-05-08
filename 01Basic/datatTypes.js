276eddc98e973b88fb67e02334ac39991fecd9ef
Arshad3620 <ridoy3620@gmail.com>
2025-05-08 20:55:45 +0900

    datatypes

---
+++ 01Basic/datatTypes.js
@@ -0,0 +1,13 @@
+"use stirct"; // treat all js code as newer version
+
+let name = "John Doe"; // String Literal
+let age = 30; // Number Literal
+let isEmployed = true; // Boolean Literal
+let salary = null; // Null Literal standulone value
+let undefinedValue; // Undefined Literal
+let symbol = Symbol("unique"); // Symbol Literal
+let bigIntValue = 1234567890123456789012345678901234567890n; // BigInt Literal
+let object = { name: "John", age: 30 }; // Object Literal
+let array = [1, 2, 3, 4, 5]; // Array Literal
+let address; // Undefined Literal
+let hobbies = ["reading", "traveling", "cooking"]; // Array Literal
