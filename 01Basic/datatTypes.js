2a3c7173c871a3705b9d18a36f895043d2a10a06
Arshad3620 <ridoy3620@gmail.com>
2025-05-11 20:32:16 +0900

    add some datatype exemple

--- 01Basic/datatTypes.js
+++ 01Basic/datatTypes.js
@@ -16,12 +16,12 @@ let array = [1, 2, 3, 4, 5]; // Array
 function greet() {
   return "Hello";
 } // Function
-let date = new Date(); // Date
-let regex = /[a-z]/; // RegExp
-let map = new Map(); // Map
-let set = new Set(); // Set
-let weakMap = new WeakMap(); // WeakMap
-let weakSet = new WeakSet(); // WeakSet
+let date = new Date(); // Date use for date and time
+let regex = /[a-z]/; // RegExp use for regular expressions
+let map = new Map(); // Map use for key-value pairs
+let set = new Set(); // Set use
+let weakMap = new WeakMap(); // WeakMap use for memory management
+let weakSet = new WeakSet(); // WeakSet use for memory management
 
 // JSON example
-let jsonString = '{"name": "John", "age": 30}';
+let jsonString = '{"name": "John", "age": 30}'; // JSON string use for data exchange
