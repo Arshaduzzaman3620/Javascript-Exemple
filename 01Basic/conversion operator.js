5c70863b45c330a095fbc7dc885079ef53f8f540
Arshad3620 <ridoy3620@gmail.com>
2025-05-11 20:43:40 +0900

    write some exm

---
+++ 01Basic/conversion operator.js
@@ -0,0 +1,24 @@
+// // Explicit Conversion Examples
+// let str = "42";
+// let num = Number(str);
+// console.log(num); // Output: 42
+
+// let bool = Boolean(0);
+// console.log(bool); // Output: false
+
+// let floatStr = "3.14";
+// let floatNum = parseFloat(floatStr);
+// console.log(floatNum); // Output: 3.14
+
+// // Implicit Conversion Examples
+// console.log("5" + 5); // Output: "55" (string concatenation)
+// console.log("5" - 2); // Output: 3 (string is coerced to a number)
+// console.log(true + 1); // Output: 2 (true is coerced to 1)
+
+
+let score ="33"
+
+console.log(typeof score );
+
+let  valueInNumber = Number(score);
+console.log(typeof valueInNumber);
