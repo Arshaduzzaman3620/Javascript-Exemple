d204252169a963a367d7da53c01fe102348a13aa
Arshad3620 <ridoy3620@gmail.com>
2025-05-15 19:46:26 +0900

    add some exm

--- 01Basic/07_Dates.js
+++ 01Basic/07_Dates.js
@@ -7,3 +7,10 @@ console.log(myDate.toLocaleDateString()); // Current date
 console.log(myDate.toLocaleTimeString()); // Current time
 let mycreateDate = new Date(2025, 0, 1); // January 1, 2025
 console.log(mycreateDate.toDateString()); // Current date and time
+
+let myTimeStamp = new Date(2025, 0, 1).getTime(); // January 1, 2025
+console.log(myTimeStamp); // Current date and time
+
+
+let newDate = new Date();
+console.log(newDate.getMonth()); // Current month
