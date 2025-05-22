083a81933dc53c4aaa8384474005daa5fc8df7b7
Arshad3620 <ridoy3620@gmail.com>
2025-05-22 20:50:54 +0900

    add some of the truthy example

--- control_flow/truthy.js
+++ control_flow/truthy.js
@@ -6,6 +6,17 @@ if (userEmail){
   console.log("Dont have user email");
 }
 
-falsy values
+// falsy values
 
-false,0, -0,BigInt 0n,"",null,undefined,NaN
+false,0, -0,BigInt ,"",null,undefined,NaN
+
+// truthy values
+// "0" ,'false',"",[],{},function(){}
+
+const emtyobject ={}
+if (Object.keys(emtyobject)){
+
+}
+
+const iceTea = 100
+iceTeaPrice >= 80 ? console.log("less then 80"); 
